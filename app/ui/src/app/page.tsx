cb129420887d786237365450167dd6e80c1985cd
Guritfak Gill <guritfak2007@gmail.com>
2026-03-25 23:49:38 -0700

    fix: remove boilerplate next.js code with view edit page button

--- app/ui/src/app/page.tsx
+++ app/ui/src/app/page.tsx
@@ -1,6 +1,14 @@
-import AddressGeocoder from './components/AddressGeocoder';
+import Link from 'next/link';
 
 export default function Home() {
-  return <AddressGeocoder />;
-  
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center gap-2">      
+      <Link 
+      href="/edit"
+      className="h-11 px-6 rounded-full border border-zinc-300 bg-white text-black text-base font-normal hover:bg-zinc-400/30 transition-colors inline-flex items-center justify-center"
+      >
+      Edit
+      </Link>
+    </div>
+  );
 }
