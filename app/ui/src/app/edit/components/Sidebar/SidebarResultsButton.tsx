2d042b8896930598fb0e184cea488fbdb5f510dc
Guritfak Gill <guritfak2007@gmail.com>
2026-04-25 22:38:33 -0700

    feat: create new sidebar component

---
+++ app/ui/src/app/edit/components/Sidebar/SidebarResultsButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  SIDEBAR_NAV_ITEM,
+  SIDEBAR_NAV_LABEL_ACTIVE,
+  SIDEBAR_NAV_LABEL_INACTIVE,
+  SIDEBAR_NAV_PILL_ACTIVE,
+  SIDEBAR_NAV_PILL_INACTIVE,
+} from "../../formStyles.v2";
+
+export default function SidebarResultsButton() {
+  const pathname = usePathname();
+  const isActive = pathname === "/results";
+
+  return (
+    <Link href="/results" className={SIDEBAR_NAV_ITEM}>
+      <span className={isActive ? SIDEBAR_NAV_PILL_ACTIVE : SIDEBAR_NAV_PILL_INACTIVE}>
+        <img src="/icons/sidebar/map_search.png" alt="" width={24} height={24} />
+      </span>
+      <span className={isActive ? SIDEBAR_NAV_LABEL_ACTIVE : SIDEBAR_NAV_LABEL_INACTIVE}>
+        Results
+      </span>
+    </Link>
+  );
+}
