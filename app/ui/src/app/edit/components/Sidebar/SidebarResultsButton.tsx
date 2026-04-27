5969af9df1a81d3bb09d4c8cf3dd9cc69780e305
Guritfak Gill <guritfak2007@gmail.com>
2026-04-27 15:59:58 -0700

    chore: remove dead variables in sidebar results button

--- app/ui/src/app/edit/components/Sidebar/SidebarResultsButton.tsx
+++ app/ui/src/app/edit/components/Sidebar/SidebarResultsButton.tsx
@@ -1,10 +1,7 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import {
   SIDEBAR_NAV_ITEM,
-  SIDEBAR_NAV_LABEL_ACTIVE,
   SIDEBAR_NAV_LABEL_INACTIVE,
-  SIDEBAR_NAV_PILL_ACTIVE,
   SIDEBAR_NAV_PILL_INACTIVE,
 } from "../../formStyles.v2";
 
